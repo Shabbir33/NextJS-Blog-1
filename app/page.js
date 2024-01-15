c98bd846aa4deec7991883db08f406b7ec34e632
Shabbir33 <shabbirpoonawala03@gmail.com>
2024-01-15 14:12:31 +0530

    Initialized the Project and added custom styles to global.css

---
+++ app/page.js
@@ -0,0 +1,49 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <Link href="/">Home</Link>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut
+        bibendum libero, nec fermentum neque. Sed a pulvinar nisl. Quisque
+        pulvinar arcu felis, vitae viverra orci tincidunt a. Phasellus at
+        porttitor quam. Nulla nunc neque, sagittis id dictum vitae, pulvinar sed
+        leo. Nulla sem magna, dignissim nec placerat eget, imperdiet vitae urna.
+        Nulla aliquet mi id lacus vestibulum, in aliquet urna cursus. Proin in
+        dapibus nunc, ac feugiat dui. Donec pharetra faucibus arcu, eu
+        consectetur nunc condimentum sed. Vestibulum euismod viverra risus id
+        vehicula. Mauris condimentum varius risus id tristique. Maecenas non
+        feugiat urna. Nullam fermentum tincidunt massa, quis hendrerit sem
+        dictum ac. Duis maximus imperdiet enim at semper. Suspendisse
+        condimentum lacinia finibus. Ut hendrerit ex a ex gravida porttitor. Sed
+        blandit elit orci, vitae tincidunt turpis ullamcorper vel. Curabitur
+        iaculis id lacus vitae tincidunt. Pellentesque habitant morbi tristique
+        senectus et netus et malesuada fames ac turpis egestas. Proin lobortis
+        faucibus porttitor. Etiam sed lectus elit. Orci varius natoque penatibus
+        et magnis dis parturient montes, nascetur ridiculus mus. Maecenas
+        ullamcorper, ex sit amet convallis egestas, metus turpis dapibus mi, vel
+        condimentum nisl felis non metus. Fusce blandit ex at pulvinar
+        fermentum. Sed pharetra turpis quis varius scelerisque. Vestibulum
+        imperdiet sem elit, molestie pretium tortor pretium ut. Sed commodo ut
+        quam ut aliquam. Quisque pharetra ligula nec est vestibulum, sagittis
+        ullamcorper sapien aliquet. Morbi scelerisque, nisi et ultrices finibus,
+        augue leo fringilla eros, at feugiat mi ipsum ac sapien. Maecenas sit
+        amet risus ex. Pellentesque ullamcorper eget nunc luctus auctor. Nam in
+        tellus non nisl consequat porttitor sit amet eu dui. Vestibulum sapien
+        metus, lacinia ac ex at, pulvinar elementum enim. Nullam finibus, leo id
+        finibus imperdiet, velit lacus pellentesque ex, id molestie ligula
+        lectus et turpis. Quisque ligula sapien, commodo sed cursus vel, auctor
+        in urna. Maecenas viverra nibh at ligula accumsan molestie. Donec
+        consequat purus erat, sed rutrum massa pretium id. Aliquam laoreet risus
+        est, a fringilla mauris blandit bibendum. Nam ullamcorper vehicula ex,
+        iaculis pulvinar nibh. Integer iaculis viverra mi, id vehicula nisl
+        molestie luctus. Proin convallis ex justo, eu tincidunt justo aliquam
+        ut. Aenean ut gravida velit. In hac habitasse platea dictumst.
+        Pellentesque habitant morbi tristique senectus et netus et malesuada
+        fames ac turpis egestas. Pellentesque convallis rutrum efficitur. Sed a
+        neque faucibus, accumsan nibh nec, rutrum lorem.
+      </p>
+    </div>
+  );
+}
